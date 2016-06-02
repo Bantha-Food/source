5315d9c4aa7569069fe482e10726ab3c8c57dd72
Bantha-Food <stefanmielke@gmail.com>
2016-06-02 12:18:35 +0200

    added toke

--- basicBot.js
+++ basicBot.js
@@ -2851,6 +2851,19 @@
                     }
                 }
             },
+            
+            tokeCommand: {
+                command: 'toke',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.blazeit)
+                    }
+                }
+            },
 
             refreshCommand: {
                 command: 'refresh',
